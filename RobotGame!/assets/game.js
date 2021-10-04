d6c7161ac6502acb361da8fa811f96b5da8afa40
LazyLizard95 <51461957+LazyLizard95@users.noreply.github.com>
2021-10-04 17:11:39 -0400

    Update game.js
    added nested statement and prompt to fight or skip

--- RobotGame!/assets/game.js
+++ RobotGame!/assets/game.js
@@ -1,4 +1,5 @@
 var playerName = window.prompt("Please enter your robot's name");
+var promptFight = window.prompt("Would you like to FIGHT or BRIBE?");
 var playerHealth = 100;
 var playerAttack = 10;
 
@@ -10,34 +11,52 @@ var enemyAttack = 12;
 
 var fight = function() {
     window.alert("Welcome to Robot Gladiators");
-    enemyHealth = enemyHealth - playerAttack;
-    playerHealth = playerHealth - enemyAttack;
-    console.log(
+    
+    promptFight.toUpperCase();
+    if(promptFight){
+        if(promptFight === "FIGHT"){
+            enemyHealth = enemyHealth - playerAttack;
+             console.log(
+                    playerName + " attacked" + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining!"
+             );
+        //check enemy hp
+        if (enemyHealth <= 0){
+            window.alert(enemyName + " has died!");
+        } else {
+            window.alert(enemyName + " still has " + enemyHealth + " health left.");
+        }
+        
+    //remove player hp
+        playerHealth = playerHealth - enemyAttack;
+        console.log(
         playerName + " attacked" + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining!"
-
-    );
-
+ );       
+    }else if (promptFight === "BRIBE"){
+        window.alert(playerName + " has paid off " + enemyName + "!");
+    } else {
+        window.alert("Does not compute! Please enter a valid option!");
+    }
+    }
     
     
-
-
+    
+    
+    
 };
 
+// if (enemyHealth <= 0) {
+//     window.alert(enemyName + " has died!");
 
+// }
+// else {
+//     window.alert(enemyName + " still has " + enemyHealth + " health left.");
+// }
 
-if (enemyHealth <= 0) {
-    window.alert(enemyName + " has died!");
+// if (playerHealth <= 0) {
+//     window.alert(playerName + " has died!");
 
-}
-else {
-    window.alert(enemyName + " still has " + enemyHealth + " health left.");
-}
-
-if (playerHealth <= 0) {
-    window.alert(playerName + " has died!");
-
-}
-else {
-    window.alert(playerName + " still has " + playerHealth + " health left.");
-}
+// }
+// else {
+//     window.alert(playerName + " still has " + playerHealth + " health left.");
+// }
 
