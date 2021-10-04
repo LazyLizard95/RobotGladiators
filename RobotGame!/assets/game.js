69832e1a55ce7cf48a8818d49c2215b2e875257d
LazyLizard95 <51461957+LazyLizard95@users.noreply.github.com>
2021-10-04 17:44:52 -0400

    Update game.js
    added confirm bribes

--- RobotGame!/assets/game.js
+++ RobotGame!/assets/game.js
@@ -1,8 +1,7 @@
 var playerName = window.prompt("Please enter your robot's name");
-
 var playerHealth = 100;
 var playerAttack = 10;
-
+var playerMoney = 10;
 console.log(playerName, playerAttack, playerHealth);
 
 var enemyName = "Roborto";
@@ -32,7 +31,15 @@ var fight = function() {
         playerName + " attacked" + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining!"
  );       
     }else if (promptFight === "BRIBE"){
+        //confirm player decision
+        var confirmBribe = window.confirm("After groveling to " + enemyName + " it's been decided that the amount of 2.00 will provide mercy. Is this okay?")
+        if (confirmBribe) {
         window.alert(playerName + " has paid off " + enemyName + "!");
+        //Remove money from player
+        playerMoney = playerMoney - 2;
+        } else{
+            fight();
+        }
     } else {
         window.alert("Does not compute! Please enter a valid option!");
     }
