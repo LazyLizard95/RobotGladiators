a438d6c922f79d7c1d73e2d49676df8612afe6cb
LazyLizard95 <51461957+LazyLizard95@users.noreply.github.com>
2021-10-06 15:19:22 -0400

    Update game.js
    Added start game function

--- RobotGame!/assets/game.js
+++ RobotGame!/assets/game.js
@@ -3,26 +3,39 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-
-
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
+//function that starts a new game
+var startGame = function() { 
+    //set player base stats
+    var playerHealth = 100;
+    var playerAttack = 10;
+    var playerMoney = 10;
 
-
+    for(i = 0; i < enemyNames.length; i++){
+        if (playerHealth > 0){
+        var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    fight(pickedEnemyName);
+    }
+    else {
+        window.alert(playerName + " has suffered critical damage! Game Over!");
+        break;
+    }
+  }
+}
+//end game function that displays stats.
+var endGame = function(){
+    window.alert("After hauling " + playerName + " to the scrapyard you took some time to reflect.");
+};
 
 var fight = function(enemyName) {
-    if (playerHealth > 0) {
-        window.alert("Welcome to Robot Gladiators, " + playerName + " Round: " + ( i + 1) + " You have " + playerHealth + " HitPoints remaining!");
-    //pick new enemy to fight based on index in array
-    var pickedEnemyName = enemyNames[i];
-    //reset enemyhealth before starting a fight
-    enemyHealth = 50;
+    window.alert("Welcome to Robot Gladiators, " + playerName + "." + " Round: " + ( i + 1) + " You have " + playerHealth + " HitPoints remaining!" + "You currently have " + playerMoney + " Buckeroos!");
     //debugger will pause the script to check whats going on
     debugger;
     //pass the pickedEnemyName var into the fight function. It wil assume the value of the enemyName parameter. 
-    
-        while(enemyHealth > 0){
+    while(enemyHealth > 0){
     var promptFight = window.prompt("Would you like to FIGHT or BRIBE?");
     promptFight = promptFight.toUpperCase(); //converts players input to uppercase
     if(promptFight){
@@ -61,14 +74,6 @@ var fight = function(enemyName) {
         window.alert("Does not compute! Please enter a valid option!");
     }
     }
-    }
-} else {window.alert(playerName + " has died!");}
-};
-
-for(i = 0; i < enemyNames.length; i++){
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
-    fight(pickedEnemyName);
 }
-
-
+};  
+startGame(); //initiate game
